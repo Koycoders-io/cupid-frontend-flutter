4ef1532625bf4d5cc708fe8963185bf3dda49d5c
Muhammed Ameen <ameens.in19@gmail.com>
2023-02-13 03:47:05 +0530

    confession screen

--- lib/utils/image_class.dart
+++ lib/utils/image_class.dart
@@ -1,5 +1,5 @@
 class ImageClass {
-  static String ulPark = 'images/ulpark.png';
-  static String businesspark = 'images/businesspark.png';
-  static String sahyapark = 'images/sahya.png';
+  static String ulPark = 'assets/images/ulpark.png';
+  static String businesspark = 'assets/images/businesspark.png';
+  static String sahyapark = 'assets/images/sahya.png';
 }
